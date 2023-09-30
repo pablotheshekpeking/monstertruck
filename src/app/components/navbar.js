3e82c61ed29023271c83bb64644e8a6ea711ee5c
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 23:40:33 +0300

    again

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,11 +8,11 @@ const Navbar = () => {
     return ( 
         <Stack color={'white'} direction={['column', 'column', 'row', 'row']} bg={'#3d3d50'} w={'100%'} pr={'50px'} pl={'50px'} pt={'20px'} pb={'20px'} justify={'center'}>
             <Box><Img /></Box>
-            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'50px'}>PRODUCTS & SERVICES</Text></Box></Link>
-            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'50px'}>ABOUT</Text></Box></Link>
-            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'50px'}>CONTACT</Text></Box></Link>
-            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'50px'}>PRESS & EVENTS</Text></Box></Link>
-            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'50px'}>FIND A DEALER</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'30px'}>PRODUCTS & SERVICES</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'30px'}>ABOUT</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'30px'}>CONTACT</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'30px'}>PRESS & EVENTS</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text p={'30px'}>FIND A DEALER</Text></Box></Link>
         </Stack> 
      );
 }
