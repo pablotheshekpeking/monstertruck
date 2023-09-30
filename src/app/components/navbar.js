46efaa7ae5dfd43c0a624c35522328aeb3b5d22b
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 23:18:18 +0300

    go

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,16 +2,17 @@
 
 import React from 'react';
 import { Stack, Box, Text, Heading, Img } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const Navbar = () => {
     return ( 
-        <Stack direction={['column', 'column', 'row', 'row']} bg={'#3d3d50'} w={'100%'} pr={'50px'} pl={'50px'} pt={'20px'} pb={'20px'}>
+        <Stack direction={['column', 'column', 'row', 'row']} bg={'#3d3d50'} w={'100%'} pr={'50px'} pl={'50px'} pt={'20px'} pb={'20px'} justify={'center'}>
             <Box><Img /></Box>
-            <Box _hover={{ backgroundColor: 'red',}}><Text>PRODUCTS & SERVICES</Text></Box>
-            <Box _hover={{ backgroundColor: 'red',}}><Text>ABOUT</Text></Box>
-            <Box _hover={{ backgroundColor: 'red',}}><Text>CONTACT</Text></Box>
-            <Box _hover={{ backgroundColor: 'red',}}><Text>PRESS & EVENTS</Text></Box>
-            <Box _hover={{ backgroundColor: 'red',}}><Text>FIND A DEALER</Text></Box>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text>PRODUCTS & SERVICES</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text>ABOUT</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text>CONTACT</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text>PRESS & EVENTS</Text></Box></Link>
+            <Link href={'../'}><Box _hover={{ backgroundColor: 'red',}}><Text>FIND A DEALER</Text></Box></Link>
         </Stack>
      );
 }
--- src/app/provider.js
+++ src/app/provider.js
@@ -4,12 +4,14 @@ import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from './components/dummy';
 import Navbar from './components/navbar';
+import Hero from './components/hero';
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
         <Navbar />
+        <Hero />
         <Home />
       </ChakraProvider>
     </CacheProvider>
