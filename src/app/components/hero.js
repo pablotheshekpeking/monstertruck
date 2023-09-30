f892ad6ee3a0be270a52339ada1ac7e768a9cd8e
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-30 23:52:13 +0300

    maybe? fingers crossed it works

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,40 +1,88 @@
 'use client'
 
-import React from "react";
-import { Stack, Box, Text, Heading, Img, Button } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Stack,
+  Box,
+  Text,
+  Heading,
+  Img,
+  Button,
+  Video,
+} from '@chakra-ui/react';
 
 const Hero = () => {
-    return (
-        <Stack
-            minH={'100vh'}
-            backgroundImage={
-                'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://media.istockphoto.com/id/1252703616/photo/monster-truck.jpg?s=612x612&w=0&k=20&c=k3tT3tVygsZWdU-AemqBCoHGO7GnfUyy98QJIKXtg3Q=)'
-            }
-            backgroundSize={'cover'}
-            backgroundPosition={'center center'}
-            color={'white'}
-        >
-            <Box m={'auto'}>
-                <Stack direction={'column'}>
-                    <Box>
-                        <Stack direction={'row'}>
-                            <Box borderRight={'1px solid'} borderColor={'#dddddd'} pr={'10px'} fontWeight={'bold'}><Text>Monster Trucks</Text></Box>
-                            <Box><Text>Heavy Duty</Text></Box>
-                        </Stack>
-                    </Box>
-                    <Box pt={'20px'}><Heading>MONSTER Truck. Bringing chaos to the streets.</Heading></Box>
-                    <Box>
-                        <Text pb={'20px'} pt={'20px'}>The brand that will make your transition to noise successful.
-                            Our trucks and services are built from REAL MONSTER experience.
-                        </Text>
-                        <Button pr={'50px'} pl={'50px'} pt={'20px'} pb={'20px'} fontWeight={'bold'} bg={'#DE522E'} color={'white'}>
-                            READ MORE
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
+  const [showVideo, setShowVideo] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowVideo(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowVideo(false);
+  };
+
+  return (
+    <Stack
+      minH={'100vh'}
+      backgroundImage={
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://media.istockphoto.com/id/1252703616/photo/monster-truck.jpg?s=612x612&w=0&k=20&c=k3tT3tVygsZWdU-AemqBCoHGO7GnfUyy98QJIKXtg3Q=)'
+      }
+      backgroundSize={'cover'}
+      backgroundPosition={'center center'}
+      color={'white'}
+    >
+      <Box m={'auto'}>
+        <Stack direction={'column'}>
+          <Box>
+            <Stack direction={'row'}>
+              <Box
+                borderRight={'1px solid'}
+                borderColor={'#dddddd'}
+                pr={'10px'}
+                fontWeight={'bold'}
+              >
+                <Text>Monster Trucks</Text>
+              </Box>
+              <Box>
+                <Text>Heavy Duty</Text>
+              </Box>
+            </Stack>
+          </Box>
+          <Box pt={'20px'}>
+            <Heading>MONSTER Truck. Bringing chaos to the streets.</Heading>
+          </Box>
+          <Box>
+            <Text pb={'20px'} pt={'20px'}>
+              The brand that will make your transition to noise successful.
+              Our trucks and services are built from REAL MONSTER experience.
+            </Text>
+            <Button
+              pr={'50px'}
+              pl={'50px'}
+              pt={'20px'}
+              pb={'20px'}
+              fontWeight={'bold'}
+              bg={'#DE522E'}
+              color={'white'}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              READ MORE
+            </Button>
+            {showVideo && (
+              <Video
+                width="100%"
+                autoPlay
+                controls
+                src="https://www.youtube.com/watch?v=3qMRaIJ3D3o" // Replace with your video source
+              />
+            )}
+          </Box>
         </Stack>
-    );
-}
+      </Box>
+    </Stack>
+  );
+};
 
 export default Hero;
