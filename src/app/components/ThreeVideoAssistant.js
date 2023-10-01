a6982a7a522f0d413a766c6687ed6f05f8068a13
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 09:54:53 +0300

    work?

---
+++ src/app/components/ThreeVideoAssistant.js
@@ -0,0 +1,34 @@
+// components/ThreeVideoAssistant.js
+import React, { useRef, useMemo } from 'react';
+import { useFrame } from 'react-three-fiber';
+import * as THREE from 'three';
+
+function ThreeVideoAssistant({ showVideo }) {
+  const videoRef = useRef();
+
+  // Create a texture from a video element
+  const videoTexture = useMemo(() => {
+    const texture = new THREE.VideoTexture(videoRef.current);
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.format = THREE.RGBFormat;
+    return texture;
+  }, []);
+
+  useFrame(() => {
+    if (showVideo) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  });
+
+  return (
+    <mesh>
+      <planeBufferGeometry args={[16, 9]} />
+      <meshBasicMaterial map={videoTexture} />
+    </mesh>
+  );
+}
+
+export default ThreeVideoAssistant;
