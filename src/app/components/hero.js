45e0a92b0338b971d995895f9d80db939f725393
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 11:28:45 +0300

    im usign normal assistant please

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -2,78 +2,80 @@
 
 import React, { useState } from 'react';
 import {
-  Stack,
-  Box,
-  Text,
-  Heading,
-  Button,
-  Tooltip, // Import Tooltip
+    Stack,
+    Box,
+    Text,
+    Heading,
+    Button,
+    Tooltip, // Import Tooltip
 } from '@chakra-ui/react';
 import Navbar from './navbar';
 
 const Hero = () => {
-  const [showTooltip, setShowTooltip] = useState(false);
+    const [showTooltip, setShowTooltip] = useState(false);
 
-  return (
-    <Stack
-      minH={'100vh'}
-      backgroundImage={
-        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://media.istockphoto.com/id/1252703616/photo/monster-truck.jpg?s=612x612&w=0&k=20&c=k3tT3tVygsZWdU-AemqBCoHGO7GnfUyy98QJIKXtg3Q=)'
-      }
-      backgroundSize={'cover'}
-      backgroundPosition={'center center'}
-      color={'white'}
-    >
-      <Navbar />
-      <Box m={'auto'}>
-        <Stack direction={'column'}>
-          <Box>
-            <Stack direction={'row'}>
-              <Box
-                borderRight={'1px solid'}
-                borderColor={'#dddddd'}
-                pr={'10px'}
-                fontWeight={'bold'}
-              >
-                <Text>Monster Trucks</Text>
-              </Box>
-              <Box>
-                <Text>Heavy Duty</Text>
-              </Box>
-            </Stack>
-          </Box>
-          <Box pt={'20px'}>
-            <Heading>MONSTER Truck. Bringing chaos to the streets.</Heading>
-          </Box>
-          <Box>
-            <Text pb={'20px'} pt={'20px'}>
-              The brand that will make your transition to noise successful.
-              Our trucks and services are built from REAL MONSTER experience.
-            </Text>
-            <Tooltip 
-              label="Click to learn more about us" // Tooltip content
-              isOpen={showTooltip} 
-              placement="bottom"
-            >
-              <Button
-                pr={'50px'}
-                pl={'50px'}
-                pt={'20px'}
-                pb={'20px'}
-                fontWeight={'bold'}
-                bg={'#DE522E'}
-                color={'white'}
-                onMouseEnter={() => setShowTooltip(true)} 
-                onMouseLeave={() => setShowTooltip(false)} 
-              >
-                READ MORE
-              </Button>
-            </Tooltip>
-          </Box>
+    return (
+        <Stack
+            minH={'100vh'}
+            backgroundImage={
+                'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://media.istockphoto.com/id/1252703616/photo/monster-truck.jpg?s=612x612&w=0&k=20&c=k3tT3tVygsZWdU-AemqBCoHGO7GnfUyy98QJIKXtg3Q=)'
+            }
+            backgroundSize={'cover'}
+            backgroundPosition={'center center'}
+            color={'white'}
+        >
+            <Navbar />
+            <Box m={'auto'}>
+                <Stack direction={'column'}>
+                    <Box>
+                        <Stack direction={'row'}>
+                            <Box
+                                borderRight={'1px solid'}
+                                borderColor={'#dddddd'}
+                                pr={'10px'}
+                                fontWeight={'bold'}
+                            >
+                                <Text>Monster Trucks</Text>
+                            </Box>
+                            <Box>
+                                <Text>Heavy Duty</Text>
+                            </Box>
+                        </Stack>
+                    </Box>
+                    <Box pt={'20px'}>
+                        <Heading>MONSTER Truck. Bringing chaos to the streets.</Heading>
+                    </Box>
+                    <Box>
+                        <Text pb={'20px'} pt={'20px'}>
+                            The brand that will make your transition to noise successful.
+                            Our trucks and services are built from REAL MONSTER experience.
+                        </Text>
+                        <Tooltip
+                            hasArrow
+                            label="Click to learn more about us" // Tooltip content
+                            isOpen={showTooltip}
+                            placement="bottom"
+                            p={'20px'}
+                        >
+                            <Button
+                                pr={'50px'}
+                                pl={'50px'}
+                                pt={'20px'}
+                                pb={'20px'}
+                                fontWeight={'bold'}
+                                bg={'#DE522E'}
+                                color={'white'}
+                                onMouseEnter={() => setShowTooltip(true)}
+                                onMouseLeave={() => setShowTooltip(false)}
+                            >
+                                READ MORE
+                            </Button>
+                        </Tooltip>
+                    </Box>
+                </Stack>
+            </Box>
         </Stack>
-      </Box>
-    </Stack>
-  );
+    );
 };
 
 export default Hero;
