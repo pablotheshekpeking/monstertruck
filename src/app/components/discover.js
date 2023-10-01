c86e8938feba53350046337556f726e453d284ce
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 18:18:43 +0300

    discover 0

---
+++ src/app/components/discover.js
@@ -0,0 +1,18 @@
+'use client'
+
+import React from "react";
+import { Stack, Box, Text, Heading, Img } from "@chakra-ui/react";
+const Discover = () => {
+    return ( 
+        <Stack pt={'50px'} pb={'50px'}>
+            <Heading textAlign={'center'} pb={'50px'}>Discover why we are the future of transportation</Heading>
+            <Stack direction={'row'} m={'auto'}>
+                <Box><Img src='/125.png' /></Box>
+                <Box><Img src='/5M.png' /></Box>
+                <Box><Img src='/0-Emission.png' /></Box>
+            </Stack>
+        </Stack>
+     );
+}
+ 
+export default Discover;
