70f3cbebce86761187a0fe997aa37d2041c4f3e7
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 18:20:21 +0300

    again

--- src/app/provider.js
+++ src/app/provider.js
@@ -3,12 +3,15 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import Hero from './components/hero';
+import Discover from './components/discover';
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
         <Hero />
+
+        <Discover />
       </ChakraProvider>
     </CacheProvider>
   );
