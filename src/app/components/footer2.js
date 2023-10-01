bfb4113839e43c4c6451540d2d33752019eec675
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-01 19:16:41 +0300

    again

--- src/app/components/footer2.js
+++ src/app/components/footer2.js
@@ -4,10 +4,10 @@ import React from "react";
 import { Stack, Box, Text } from "@chakra-ui/react";
 const Footer2 = () => {
     return ( 
-        <Stack direction={'row'} justify={'center'} bg={'#3D3D3D'} p={'10px'}>
-            <Text>Terms of Use</Text>
-            <Text>Privacy Statement</Text>
-            <Text>Cookies Statement</Text>
+        <Stack direction={'row'} justify={'center'} bg={'#3D3D3D'} p={'10px'} color={'#E7DFCC'} fontSize={['8px', '8px', '15px', '15px']}>
+            <Text cursor={'pointer'} _hover={{ color: 'black', }}>Terms of Use</Text>
+            <Text cursor={'pointer'} _hover={{ color: 'black', }}>Privacy Statement</Text>
+            <Text cursor={'pointer'} _hover={{ color: 'black', }}>Cookies Statement</Text>
             <Text>© 2023 MFTBC. All rights reserved. MONSTER is a Daimler Truck Group Brand.</Text>
         </Stack>
      );
