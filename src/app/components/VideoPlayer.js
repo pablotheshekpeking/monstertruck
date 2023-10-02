e9aecd2d5645605aab29518790e56216bb34daab
Michael Ibuaku <mujiangelo17@gmail.com>
2023-10-02 10:51:40 +0300

    done

--- src/app/components/VideoPlayer.js
+++ src/app/components/VideoPlayer.js
@@ -24,20 +24,42 @@ const VideoPlayer = () => {
   };
 
   return (
-    <video
-      ref={videoRef}
-      src="/assistant.mov" // Replace with the path to your video file
-      autoPlay
-      playsInline // Required for autoplay on some devices
-      onEnded={handleVideoEnded} // Pause the video when it ends
-      style={{
-        position: 'fixed',
-        bottom: '0',
-        left: '5px',
-        width: '200px', // Set the desired width
-        zIndex: '9999', // Adjust the z-index to be above other content
-      }}
-    />
+    <div>
+      <video
+        ref={videoRef}
+        src="/assistant.mov" // Replace with the path to your video file
+        autoPlay
+        playsInline // Required for autoplay on some devices
+        onEnded={handleVideoEnded} // Pause the video when it ends
+        controls // Show video controls
+        style={{
+          position: 'fixed',
+          bottom: '0',
+          left: '5px',
+          width: '200px', // Set the desired width
+          zIndex: '9999', // Adjust the z-index to be above other content
+        }}
+      />
+      <style jsx>{`
+        /* Customize the appearance of video controls */
+        video::-webkit-media-controls-panel {
+          display: flex;
+          justify-content: center;
+          background: transparent;
+          color: transparent;
+        }
+        video::-webkit-media-controls-play-button {
+          display: initial; /* Show only the play button */
+        }
+        /* Hide other controls */
+        video::-webkit-media-controls-current-time-display,
+        video::-webkit-media-controls-time-remaining-display,
+        video::-webkit-media-controls-mute-button,
+        video::-webkit-media-controls-volume-slider {
+          display: none;
+        }
+      `}</style>
+    </div>
   );
 };
 
